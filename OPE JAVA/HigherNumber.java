e811e28bf5aac3dc927fa3b44174e0dad52a879b
Abdulrasaq1515 <rasaqajape@gmail.com>
2025-05-02 12:48:25 +0100

    Create HigherNumber.java

---
+++ OPE JAVA/HigherNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+	public class HigherNumber{
+
+	public static void main(String[] args){
+
+Scanner give = new Scanner(System.in);
+
+	System.out.println("Enter the integer");
+
+	int num1 = give.nextInt(), num2 = give.nextInt(), num3 = give.nextInt();
+
+
+	if(num2 > num1 && num3 > num2){
+
+	System.out.print("true");
+
+}
+
+
+
+
+
+}
+
+}
