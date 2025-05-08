0fb58b4c83870645369acb523401ee058fc657ea
Abdulrasaq1515 <rasaqajape@gmail.com>
2025-05-08 19:29:04 +0100

    assignments

--- MR SIKIRU PYTHON/Multiples.py
+++ MR SIKIRU PYTHON/Multiples.py
@@ -2,6 +2,12 @@ number = 1024
 
 if 1024 % 4 == 0:
 	print(True)
+	
+else:
+	print(false)
 		
 if 10 % 2 == 0:
 	print(True)
+	
+else: 
+	print(false)	
---
+++ MR SIKIRU PYTHON/sortingnumber.py
@@ -0,0 +1,10 @@
+num1 = float(input("enter the number : "))
+num2 = float(input("enter the number : "))
+num3 = float(input("enter the number : "))
+
+result = [num1,num2,num3]
+
+result.sort()
+
+
+print(result)
